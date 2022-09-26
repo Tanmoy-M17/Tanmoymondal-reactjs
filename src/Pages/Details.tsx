028bb83fdf4c784360ebbc5d454dc9ec9a32c9a8
Tanmoy-M17 <97627356+Tanmoy-M17@users.noreply.github.com>
2022-09-26 12:11:28 +0530

    Working on css

--- src/Pages/Details.tsx
+++ src/Pages/Details.tsx
@@ -4,16 +4,26 @@ import { useParams } from 'react-router-dom';
 import {  fetchDetails } from "../Redux/UserSlice/UserSlice";
 import { useAppDispatch, useAppSelector } from "../Redux/App/hooks";
 const Details = () => {
-  const  singledata=useAppSelector(state=>state.app.details);
+  const  Details=useAppSelector(state=>state.app.details);
     const dispach=useAppDispatch();
     const { id }  = useParams();  
     useEffect(() => {
         dispach(fetchDetails(`${id}`))
       }, [dispach,id]);
   return (
-    <div>
-      {singledata["_id"]}
+    <div  className="max-w-sm m-auto rounded overflow-hidden shadow-lg hover:bg-whitesmoke-500">
+    <img className="w-full h-2/4"  src={Details.avatar} alt="img"/>
+    <div className="px-6 py-4">
+    <div className="font-bold text-xl mb-2">{Details.name}</div>
+    <p className=" text-base text-violet-500">
+      {Details.description}
+    </p>
+    <p className="text-gray-700 text-base">
+     Price:{Details.price}
+    </p>
     </div>
+    <p>{Details.name}</p>
+</div>
   )
 }
 
--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -20,15 +20,28 @@ export const Home = () => {
 
   return <div>
     <Category/>
+    <div className="grid grid-cols-4 gap-4">
     {Products.map((e:IproductItemsProps)=>{
-        return  <div key={e._id}>
+        return  <div key={e._id} className="max-w-sm rounded overflow-hidden shadow-lg hover:bg-whitesmoke-500">
             <Link to={`/${e._id}`}> 
-            <img src={e.avatar} alt="img"></img>
+            <img className="w-full h-2/4"  src={e.avatar} alt="img"/>
+            <div className="px-6 py-4">
+            <div className="font-bold text-xl mb-2">{e.name}</div>
+            <p className="text-gray-700 text-base truncate">
+              {e.description}
+            </p>
+            </div>
             <p>{e.name}</p>
             </Link>
-            <button onClick={()=>{dispach(addTofavoutite(e))}}>Add To favoutite</button>
-            <button onClick={()=>{dispach(removeProduct(`${e._id}`))}}>Delete</button>
+            <div className="px-6 pt-4 pb-2 flex justify-between space-x-7">
+            <button className="bg-transparent hover:bg-red-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded"
+            onClick={()=>{dispach(removeProduct(`${e._id}`))}}>Delete</button>
+            <button className="bg-transparent hover:bg-green-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded" 
+            onClick={()=>{dispach(addTofavoutite(e))}}>Add To favoutite</button>
+           
+            </div>
         </div>
     })}
+    </div>
   </div>;
 };
